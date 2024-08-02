9044260e964daf95511485100097293a3e2a243c
JoJo <josephhe255@gmail.com>
2024-08-02 15:00:08 +0800

    dockerizing api

---
+++ movie-api/src/test/java/dev/joseph/MovieListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joseph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
